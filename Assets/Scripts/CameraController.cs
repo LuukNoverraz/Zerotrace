965bb5020c2bf0cce29d5baa3720731c568b2044
LuukNoverraz <luuk.noverraz@coderclass.nl>
2021-02-25 14:45:49 +0100

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public float sensitivity = 2.0f;
+    void Update()
+    {
+        player.transform.RotateAround(player.transform.position, Vector3.up, Input.GetAxis("Mouse X") * sensitivity);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform cameraTransform;
+
+    public float movementSpeed = 10f;
+    public float rotationSpeed = 30f;
+    public float deadZoneDegrees = 15f;
+
+    private Vector3 cameraDirection;
+    private Vector3 playerDirection;
+    private Quaternion targetRotation;
+
+    void Update()
+    {
+        cameraDirection = new Vector3(cameraTransform.forward.x, 0f, cameraTransform.forward.z);
+        playerDirection = new Vector3(transform.forward.x, 0f, transform.forward.z);
+
+        if (Vector3.Angle(cameraDirection, playerDirection) > 15)
+        {
+            targetRotation = Quaternion.LookRotation(cameraDirection, transform.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
